440785456b1c26a5f28999d70956d841119eef3f
nikit34 <permikov134@mail.ru>
2023-11-12 09:26:06 +0000

    Refactoring

--- cats.py
+++ cats.py
@@ -1,4 +1,4 @@
-import pets_kaggle 
+import pets_kaggle
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
@@ -8,16 +8,15 @@ test2 = pd.DataFrame(pets_kaggle.test[pets_kaggle.test['Type'] % 2 == 0])
 
 Adopttrain = pd.DataFrame(train2[['AdoptionSpeed']])
 ID = pd.DataFrame(test2[['PetID']])
-test2.drop(['PetID'],axis=1, inplace=True)
+test2.drop(['PetID'], axis=1, inplace=True)
 train2 = train2.drop(['PetID', 'AdoptionSpeed'], axis=1)
 
-
 parameter_grid = {
-            'criterion': ['entropy'],
-            'max_depth': [30,33,36,40,43,46,50],
-            'n_estimators': [40,43,46,50,53,56,60],
-            'min_samples_leaf': [5]
-        }
+    'criterion': ['entropy'],
+    'max_depth': [30, 33, 36, 40, 43, 46, 50],
+    'n_estimators': [40, 43, 46, 50, 53, 56, 60],
+    'min_samples_leaf': [5]
+}
 clf = RandomForestClassifier(n_jobs=-1)
 grid_searcher = GridSearchCV(clf, parameter_grid, verbose=2)
 grid_searcher.fit(train2, Adopttrain.values.ravel())
@@ -28,5 +27,3 @@ predictions = pd.DataFrame(clf_best.predict(test2))
 predictions.columns = [' AdoptionSpeed']
 
 cattest = ID.join(predictions)
-
-
